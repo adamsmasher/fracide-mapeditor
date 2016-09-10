fdcdd11c408b50d21ab2a6c53967c825173be550
Adam Smith <unfactorable.polynomial@gmail.com>
2016-09-10 15:08:52 -0400

    indent

--- map.h
+++ map.h
@@ -7,10 +7,10 @@
 #define MAP_TILES_HIGH  9
 
 typedef struct Map_tag {
-	char name[64];
-	/* 1-indexed */
-	UWORD tilesetNum;
-	UBYTE tiles[MAP_TILES_WIDE * MAP_TILES_HIGH];
+    char name[64];
+    /* 1-indexed */
+    UWORD tilesetNum;
+    UBYTE tiles[MAP_TILES_WIDE * MAP_TILES_HIGH];
 } Map;
 
 Map *allocMap(void);
