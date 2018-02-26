8aa6b7d29dcf2457129b4491aee32ab195ed77af
Adam Smith <unfactorable.polynomial@gmail.com>
2018-02-26 00:44:59 -0500

    do more to deal with dead windows

--- framework/runstate.c
+++ framework/runstate.c
@@ -9,15 +9,30 @@
 
 static BOOL running = TRUE;
 
+static void cleanupDeadWindows(void) {
+  FrameworkWindow *i, *next;
+
+  i = windowSetFirstWindow();
+  while(i) {
+    /* TODO: handle child windows */
+    next = i->next;
+    if(i->closed) {
+      closeWindow(i);
+    }
+    i = next;
+  }
+}
+
 void runMainLoop(void) {
   running = TRUE;
   while(running) {
     FrameworkWindow *i;
     long signalSet = Wait(windowSetSigMask());
     for(i = windowSetFirstWindow(); i != NULL; i = i->next) {
+      /* TODO: handle child windows */
       handleWindowEvents(i, signalSet);
     }
-    /* TODO: clean up any dead windows */
+    cleanupDeadWindows();
   }
 }
 
