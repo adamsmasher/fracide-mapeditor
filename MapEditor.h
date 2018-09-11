bbf51c012fb9ef605a604fa99a44ffe92ae17adc
Adam Smith <unfactorable.polynomial@gmail.com>
2018-09-11 02:20:09 -0400

    rearrange

--- MapEditor.h
+++ MapEditor.h
@@ -10,21 +10,22 @@
 #include "TilesetRequester.h"
 #include "Map.h"
 
+/* constructors */
 FrameworkWindow *newMapEditorNewMap(FrameworkWindow *parent);
 FrameworkWindow *newMapEditorWithMap(FrameworkWindow *parent, Map*, int mapNum);
 
+/* properties */
 BOOL isMapEditor(FrameworkWindow*);
 
+/* event handlers */
+/* (menus) */
 void mapEditorNewMap(FrameworkWindow*);
 void mapEditorOpenMap(FrameworkWindow*);
 BOOL mapEditorSaveMap(FrameworkWindow*);
 BOOL mapEditorSaveMapAs(FrameworkWindow*);
 void mapEditorRevertMap(FrameworkWindow*);
 
-void mapEditorRefreshRevertMap(FrameworkWindow*);
-void mapEditorRefreshTitle(FrameworkWindow*);
-
-void mapEditorUpdateMapName(FrameworkWindow*);
+/* (buttons) */
 void mapEditorChooseTilesetClicked(FrameworkWindow*);
 void mapEditorChangeSongClicked(FrameworkWindow*);
 void mapEditorClearSongClicked(FrameworkWindow*);
@@ -34,10 +35,14 @@ void mapEditorMapUpClicked(FrameworkWindow*);
 void mapEditorMapDownClicked(FrameworkWindow*);
 void mapEditorEntitiesClicked(FrameworkWindow*);
 
-void mapEditorUpdateTileDisplays(FrameworkWindow *mapEditor);
+/* refreshing */
+void mapEditorRefreshRevertMap(FrameworkWindow*);
+void mapEditorRefreshTitle(FrameworkWindow*);
+void mapEditorUpdateMapName(FrameworkWindow*);
+void mapEditorUpdateTileDisplays(FrameworkWindow*);
+void mapEditorRefreshSong(FrameworkWindow*);
 
-void mapEditorRefreshSong(FrameworkWindow *mapEditor);
-
+/* garbage */
 BOOL ensureMapEditorSaved(FrameworkWindow *mapEditor);
 
 void mapEditorDrawEntity(FrameworkWindow *mapEditor, int entityNum);
