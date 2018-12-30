c575a2788b743af47914b2c8172e16f52c6d9ff8
Adam Smith <unfactorable.polynomial@gmail.com>
2018-12-29 23:57:14 -0500

    pull this outta the h file

--- EntityBrowser.c
+++ EntityBrowser.c
@@ -99,6 +99,38 @@
 #define TAG_VALUE_LEFT       285
 #define TAG_VALUE_TOP        205
 
+#define ENTITY_LABEL_LENGTH 16
+
+typedef struct EntityBrowserGadgets_tag {
+  struct Gadget *addEntityGadget;
+  struct Gadget *removeEntityGadget;
+  struct Gadget *entityListGadget;
+  struct Gadget *rowGadget;
+  struct Gadget *colGadget;
+  struct Gadget *VRAMSlotGadget;
+  struct Gadget *addTagGadget;
+  struct Gadget *deleteTagGadget;
+  struct Gadget *chooseEntityGadget;
+  struct Gadget *tagListGadget;
+  struct Gadget *tagAliasGadget;
+  struct Gadget *tagIdGadget;
+  struct Gadget *tagValueGadget;
+  struct Gadget *thisEntityGadget;
+} EntityBrowserGadgets;
+
+typedef struct EntityBrowserData_tag {
+  char title[32];
+  int selectedEntity;
+  int selectedTag;
+  struct List entityLabels;
+  struct Node *entityNodes;
+  char (*entityStrings)[ENTITY_LABEL_LENGTH];
+  struct List tagLabels;
+  struct Node *tagNodes;
+  char (*tagStrings)[TAG_ALIAS_LENGTH + 4];
+  FrameworkWindow *entityRequester;
+} EntityBrowserData;
+
 static Result createEntityLabels(FrameworkWindow *entityBrowser) {
   /* TODO: free the old labels if necessary */
   EntityBrowserData *data = entityBrowser->data;
--- EntityBrowser.h
+++ EntityBrowser.h
@@ -1,43 +1,9 @@
 #ifndef ENTITY_BROWSER_H
 #define ENTITY_BROWSER_H
 
-#include <intuition/intuition.h>
-
 #include "framework/Window.h"
 
 #include "Map.h"
-
-#define ENTITY_LABEL_LENGTH 16
-
-typedef struct EntityBrowserGadgets_tag {
-  struct Gadget *addEntityGadget;
-  struct Gadget *removeEntityGadget;
-  struct Gadget *entityListGadget;
-  struct Gadget *rowGadget;
-  struct Gadget *colGadget;
-  struct Gadget *VRAMSlotGadget;
-  struct Gadget *addTagGadget;
-  struct Gadget *deleteTagGadget;
-  struct Gadget *chooseEntityGadget;
-  struct Gadget *tagListGadget;
-  struct Gadget *tagAliasGadget;
-  struct Gadget *tagIdGadget;
-  struct Gadget *tagValueGadget;
-  struct Gadget *thisEntityGadget;
-} EntityBrowserGadgets;
-
-typedef struct EntityBrowserData_tag {
-  char title[32];
-  int selectedEntity;
-  int selectedTag;
-  struct List entityLabels;
-  struct Node *entityNodes;
-  char (*entityStrings)[ENTITY_LABEL_LENGTH];
-  struct List tagLabels;
-  struct Node *tagNodes;
-  char (*tagStrings)[TAG_ALIAS_LENGTH + 4];
-  FrameworkWindow *entityRequester;
-} EntityBrowserData;
 
 FrameworkWindow *newEntityBrowserWithMapNum(FrameworkWindow *parent, const Map*, UWORD mapNum);
 
